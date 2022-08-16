82548b75dfa19f337cf1f1eccdc02379f2f4365d
RipperoniRepo <ripperj2019@gmail.com>
2022-08-16 15:16:00 -0400

    Initial Setup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+    for (let i = 1; i <= answer; i++) {
+      ((i%3===0)&&(i%5===0)) ? (console.log("FizzBuzz"))
+    : (i%5===0) ? (console.log("Buzz"))
+    : (i%3===0) ? (console.log("Fizz"))
+    : console.log(i);
+    }
